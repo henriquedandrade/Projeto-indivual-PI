b5222250bc9c82d46a50a55cd9190a4611daad43
henriquedandrade <henrique.barros@sptech.school>
2024-11-05 18:53:24 -0300

    Validação front-end Login 90%

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -26,7 +26,7 @@ function autenticar(req, res) {
                             senha: resultadoAutenticar[0].senha
                         });
                                                  
-                    } else if (resultadoAutenticar.length == 0) {
+                    } else if (resultadoAutenticar.length == 0) {                        
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
