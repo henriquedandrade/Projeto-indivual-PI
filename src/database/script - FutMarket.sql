4657d3acc55382b4abdf1097950bc11185c4cf43
henriquedandrade <henrique.barros@sptech.school>
2024-11-09 21:17:22 -0300

    Alguns ajustes

--- src/database/script - FutMarket.sql
+++ src/database/script - FutMarket.sql
@@ -22,3 +22,27 @@ create table tb_logacesso(
     fkusuario int,
     constraint FkAcessoUsuario foreign key (fkusuario) references tb_cadastro(idcadastro)
 );
+
+create table tb_noticias(
+idnoticia int primary key auto_increment,
+assunto varchar(50),
+chaves varchar(50),
+descricao varchar(256),
+ft_noticia varchar(256),
+dtPublicacao datetime,
+fkAutor int,
+constraint FkAutorPublica foreign key (fkAutor) references tb_cadastro(idcadastro)
+);
+
+SELECT tb_noticias.assunto AS "Assunto", 
+       tb_noticias.chaves AS "Palavras-Chave", 
+       tb_noticias.descricao AS "Descrição", 
+       tb_noticias.dtPublicacao AS "Data da Publicação", 
+       tb_cadastro.username AS "Autor da notícia"
+FROM tb_noticias
+JOIN tb_cadastro ON tb_noticias.fkAutor = tb_cadastro.idcadastro;
+
+SELECT concat(tb_cadastro.username , " logou as ", tb_logacesso.acesso ) as Acessos 
+FROM tb_logacesso
+JOIN tb_cadastro
+ON fkusuario = idcadastro;
